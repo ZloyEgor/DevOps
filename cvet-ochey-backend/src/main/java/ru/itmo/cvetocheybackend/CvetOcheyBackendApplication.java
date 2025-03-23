4880ad42d06c571fbca4dd9c1c316a91956ddb32
nesterrovv <nesterov.iv2018@gmail.com>
2025-03-23 15:47:54 +0300

    feature: initial configutation of backend

---
+++ cvet-ochey-backend/src/main/java/ru/itmo/cvetocheybackend/CvetOcheyBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.cvetocheybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvetOcheyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvetOcheyBackendApplication.class, args);
+    }
+
+}
---
+++ cvet-ochey-backend/src/test/java/ru/itmo/cvetocheybackend/CvetOcheyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.cvetocheybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvetOcheyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
