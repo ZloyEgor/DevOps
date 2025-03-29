29543bcec6322cb4d7300633cf3b3ba547cfdc5e
ZloyEgor <elopatov74@outlook.com>
2025-03-29 21:43:57 +0300

    #18: Fix build problems

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    webpack: (config) => {
-        // camelCase style names from css modules
-        config.module.rules
-            .find(({ oneOf }) => !!oneOf)
-            .oneOf.oneOf.filter(({ use }) => JSON.stringify(use)?.includes('css-loader'))
-            .reduce((acc, { use }) => acc.concat(use), [])
-            .forEach(({ options }) => {
-                if (options.modules) {
-                    options.modules.exportLocalsConvention = 'camelCase';
-                }
-            });
-
-        return config;
-    },
+    /* config options here */
 };
 
 export default nextConfig;
