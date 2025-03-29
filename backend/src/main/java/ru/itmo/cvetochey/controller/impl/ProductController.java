7235b596b6eead2d43a65474f96522b8b87ed6c9
nesterrovv <nesterov.iv2018@gmail.com>
2025-03-29 16:52:46 +0300

    ticket-13: checkstyle violations fix

--- backend/src/main/java/ru/itmo/cvetochey/controller/impl/ProductController.java
+++ backend/src/main/java/ru/itmo/cvetochey/controller/impl/ProductController.java
@@ -1,5 +1,6 @@
 package ru.itmo.cvetochey.controller.impl;
 
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.itmo.cvetochey.model.Product;
 import ru.itmo.cvetochey.repository.ProductRepository;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/cvet-ochet/api/v1/product")
