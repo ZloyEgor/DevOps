229a8778511ccbfde271885eb562fc7445bf88ec
ZloyEgor <elopatov74@outlook.com>
2025-04-16 00:34:57 +0300

    #31 Updates on ansible-playbook

--- frontend/src/entities/catalog/api/catalog-service.ts
+++ frontend/src/entities/catalog/api/catalog-service.ts
@@ -2,7 +2,7 @@ import { CatalogEntry } from '../model/catalog';
 
 export const catalogService = {
     getCatalogs: (): Promise<CatalogEntry[]> =>
-        fetch('http://localhost:8080/cvet-ochey/api/v1/catalog', { cache: 'no-store' }).then((r) =>
-            r.json()
+        fetch('http://nesterrovv.ru:8080/cvet-ochey/api/v1/catalog', { cache: 'no-store' }).then(
+            (r) => r.json()
         ),
 };
