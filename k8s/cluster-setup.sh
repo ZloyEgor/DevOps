4b336a096999af88f03fef9e75e5284480ac0bd3
ZloyEgor <elopatov74@outlook.com>
2025-09-04 23:38:41 +0300

    Created kubernetes cluster, deployed frontend and backend, set up load balancers

--- k8s/cluster-setup.sh
+++ k8s/cluster-setup.sh
@@ -6,31 +6,31 @@ echo "Creating Kubernetes cluster..."
 # Create cluster
 yc managed-kubernetes cluster create \
   --name cvetochey-cluster \
-  --network-name default \
+  --network-name cvetochey-net \
   --zone ru-central1-a \
-  --subnet-name default-ru-central1-a \
+  --subnet-name cvetochey-subnet \
   --public-ip \
   --release-channel regular \
-  --version 1.28 \
+  --version 1.30 \
   --cluster-ipv4-range 10.96.0.0/16 \
-  --service-ipv4-range 10.112.0.0/16
+  --service-ipv4-range 10.112.0.0/16 \
+  --service-account-id ajecso143s72sdd36vnd \
+  --node-service-account-id aje5ucsjf8d3s74tejva
 
 # Wait for cluster to be ready
 echo "Waiting for cluster to be ready..."
 sleep 60
 
-# Create node group
-yc managed-kubernetes node-group create \
+# Create node group with adequate resources
+yc managed-kubernetes node-group create worker-nodes \
   --cluster-name cvetochey-cluster \
-  --name worker-nodes \
   --platform standard-v3 \
-  --cores 2 \
-  --memory 4GB \
+  --cores 4 \
+  --memory 8GB \
   --disk-type network-hdd \
-  --disk-size 32GB \
-  --fixed-size 2 \
-  --location zone=ru-central1-a \
-  --public-ip
+  --disk-size 64GB \
+  --fixed-size 3 \
+  --network-interface subnets=e9bjq4eom3qadbhb9i87,ipv4-address=auto
 
 echo "Cluster setup complete!"
 
